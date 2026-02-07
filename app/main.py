1960133913aad13b56da55a19127eae1cb6d415e
Hansell Figueroa <hansell@Hansells-MacBook-Air.local>
2026-02-07 13:19:04 -0400

    init ci pipeline

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/")
+def home():
+    return {"hello": "world"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
